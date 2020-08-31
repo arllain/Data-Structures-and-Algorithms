235dbea46cbe2ea45baf2f7be676d089b655805c
Arllain <arllain@gmail.com>
2020-08-31 06:22:45 -0300

    reversing string algorithm

--- javascript/index.js
+++ javascript/index.js
@@ -1,11 +1,16 @@
 import MyArray from "./data_structures/arrays/MyArray.js";
+import ReverseString from "./algorithms/ReverseString.js";
 
-const myArray = new MyArray();
-myArray.push("hi");
-myArray.push("you");
-myArray.push("!");
-myArray.delete(0);
-myArray.push("are");
-myArray.push("nice");
-myArray.delete(1);
-console.log(myArray);
+const reverseString = new ReverseString();
+const result = reverseString.reverse("This is a reversed string");
+console.log(result.join(""));
+
+// const myArray = new MyArray();
+// myArray.push("hi");
+// myArray.push("you");
+// myArray.push("!");
+// myArray.delete(0);
+// myArray.push("are");
+// myArray.push("nice");
+// myArray.delete(1);
+// console.log(myArray);
