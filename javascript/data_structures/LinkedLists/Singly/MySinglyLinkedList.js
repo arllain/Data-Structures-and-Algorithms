f5a61f48fdbe594aecf843416ebf27733254d8f9
Arllain <arllain@gmail.com>
2020-09-07 05:32:17 -0300

    The reverse method was meant to be implemented in the Singly LinkedList

--- javascript/data_structures/LinkedLists/Singly/MySinglyLinkedList.js
+++ javascript/data_structures/LinkedLists/Singly/MySinglyLinkedList.js
@@ -70,6 +70,26 @@ class MySinglyLinkedList {
     }
     console.log(list.join(" => "));
   }
+
+  // reversing the linkedlist
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
 }
 
 export default MySinglyLinkedList;
